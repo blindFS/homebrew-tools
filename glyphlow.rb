50d6a22c4db392c4660044a45a51c3202f152ce1
blindfs <blindfs19@gmail.com>
2026-05-01 12:44:18 +0800

    fix: glyphlow download url

--- glyphlow.rb
+++ glyphlow.rb
@@ -1,7 +1,7 @@
 class Glyphlow < Formula
   desc "Feature-rich manipulation of UI elements on macOS with minimal key strokes."
   homepage "https://github.com/blindfs/glyphlow"
-  url "https://github.com/blindfs/glyphlow/archive/v0.0.6.tar.gz"
+  url "https://github.com/blindFS/Glyphlow/releases/download/v0.0.6/glyphlow.tar.gz"
   sha256 "be12f671dbfb4ebea4a1652a12820f20d5943af7d29f2164719ee740d4c21edf"
   license "MIT"
 
