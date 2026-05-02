0f2f4a1333a564bf0ed74583c92f5b56fdc99343
blindfs <blindfs19@gmail.com>
2026-05-02 12:45:17 +0800

    fix: PATH env var for glyphlow service

--- glyphlow.rb
+++ glyphlow.rb
@@ -1,8 +1,8 @@
 class Glyphlow < Formula
   desc "Feature-rich manipulation of UI elements on macOS with minimal key strokes."
   homepage "https://github.com/blindfs/glyphlow"
-  url "https://github.com/blindFS/Glyphlow/releases/download/v0.0.8/glyphlow.tar.gz?v1"
-  sha256 "4c66e64673d31a2aebee6b55694d355a816886818527d852eb2d9a2e2bdf0097"
+  url "https://github.com/blindFS/Glyphlow/releases/download/v0.0.8/glyphlow.tar.gz?v2"
+  sha256 "a33f3235479f7f5c824b5a2a7487f35488d3a149160855d862bfd9a7718e14b5"
   license "MIT"
 
   def install
@@ -13,6 +13,7 @@ class Glyphlow < Formula
     run [opt_bin/"glyphlow"]
     run_type :immediate
     keep_alive true
+    environment_variables PATH: "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:#{HOMEBREW_PREFIX}/bin"
     log_path var/"log/glyphlow.log"
     error_log_path var/"log/glyphlow.log"
   end
