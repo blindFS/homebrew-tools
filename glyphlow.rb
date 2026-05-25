9dfa1112cbd53489a09c8dcf81ec40e3af902c83
blindfs <blindfs19@gmail.com>
2026-05-25 10:03:13 +0800

    upgrade glyphlow to 0.1.1

--- glyphlow.rb
+++ glyphlow.rb
@@ -1,9 +1,9 @@
 class Glyphlow < Formula
   desc "Feature-rich manipulation of UI elements on macOS with minimal key strokes."
   homepage "https://github.com/blindfs/glyphlow"
-  url "https://github.com/blindFS/Glyphlow/releases/download/v0.1.0/glyphlow.tar.gz"
-  sha256 "d500ac90a3aefa637d06a0fc068c6d15802c5d511fc964de13543a7b5fa3d545"
-  version "0.1.0"
+  url "https://github.com/blindFS/Glyphlow/releases/download/v0.1.1/glyphlow.tar.gz"
+  sha256 "4f4e33be0b6a303c2b6e2ca58173f5d3d98a8865baa8fa4a93becf800bd9860a"
+  version "0.1.1"
   revision 1
   license "MIT"
 
