0d15cc15773e87d93d7f06db4a869480b45d3063
blindfs <blindfs19@gmail.com>
2026-09-19 18:42:27 +0800

    fix: completion

--- glyphlow-cli.rb
+++ glyphlow-cli.rb
@@ -8,10 +8,14 @@ class GlyphlowCli < Formula
 
   def install
     bin.install "glyphlow-cli"
-    generate_completions_from_executable(bin/"glyphlow-cli", "complete")
+    # `glyphlow-cli complete bash|zsh|fish` prints the script to stdout; this
+    # installs them into the usual bash/zsh/fish completion directories.
+    generate_completions_from_executable(bin/"glyphlow-cli", "complete", shells: [:bash, :zsh, :fish])
   end
 
   test do
     assert_match "Command line client", shell_output("#{bin}/glyphlow-cli --help")
+    assert_match "#compdef glyphlow-cli", shell_output("#{bin}/glyphlow-cli complete zsh")
+    assert_match "complete -c glyphlow-cli", shell_output("#{bin}/glyphlow-cli complete fish")
   end
 end
